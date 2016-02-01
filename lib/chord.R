56add94cab8ebd0074dd59a7e8a8628a40933868
habdulkafi <habdulkafi@wesleyan.edu>
2016-01-31 21:58:48 -0500

    added a tiny R Script that still needs some work (if anyone wants to work on it, i wrote what needs to be done).  Also added the data columns we're using in an rds file.

---
+++ lib/chord.R
@@ -0,0 +1,28 @@
+# setwd("../Downloads/pums")
+# 
+# a <- readRDS("pa.rds")
+# b <- readRDS("pb.rds")
+# 
+# colsToKeep <- c("FOD1P", "INDP")
+# a <- a[colsToKeep]
+# b <- b[colsToKeep]
+# 
+# c <- rbind(a,b)
+# rm(a,b)
+# saveRDS(c,"pop_degree_ind.rds")
+c <- readRDS("pop_degree_ind.rds")
+d <- na.omit(c)
+rm(c)
+# turns it into a table with columns being the Industry 
+# and the rows being the Field of Degree.
+# The cross between the two is the the "count". 
+# For example, there are 230 people who had a Degree in 1100 and work in industry 0170.
+e <- table(d$FOD1P,d$INDP)
+
+# TODO
+# Need to turn e into a data frame that looks like:
+# Industry    Degree    Count
+#   0170        1100    230
+#   0180        1101    83
+
+
