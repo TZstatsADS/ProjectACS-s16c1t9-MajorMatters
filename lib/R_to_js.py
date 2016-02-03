b332249490109275a1932e4724694f5670a95a5a
habdulkafi <habdulkafi@wesleyan.edu>
2016-02-03 12:59:05 -0500

    THE CIRCLE THING WORKS NOW

--- lib/R_to_js.py
+++ lib/R_to_js.py
@@ -1,7 +1,7 @@
 
-lines = [line.rstrip('\n\r') for line in open('bip_data_occ_deg.csv',"rb")]
+lines = [line.rstrip('\n\r') for line in open('huge_data_occ_deg.csv',"rb")]
 
-g = open("bi_dat.csv","wb")
+g = open("HUGE_dat.csv","wb")
 
 g.write("[\n")
 
@@ -44,4 +44,27 @@ for i in majors + occps:
     giant += str(big_dic[i])
     giant += ", "
 giant += "}"
+
+
+big_lis = []
+for i in sort_lis:
+	sma_lis = []
+	for j in sort_lis:
+		if i["data"] == "major":
+			first = "Major"
+		else:
+			first = "Occp"
+		if j["data"] == "major":
+			second = "Major"
+		else:
+			second = "Occp"
+		if first == second:
+			sma_lis.append(0)
+		else:
+			if pd[(pd[first] == i["name"]) & (pd[second] == j["name"])].empty:
+				sma_lis.append(0)
+			else:
+				sma_lis.append(int(pd[(pd[first] == i["name"]) & (pd[second] == j["name"])].Freq))
+	big_lis.append(sma_lis)
+
 
